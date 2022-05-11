77417f2320434c0fcb482a4d6a0d97b38db754e7
Thomas Buckley-Houston <tom@tombh.co.uk>
2022-05-11 17:49:45 -0300

    nvim: Tweaked completion menu
    I can navigate with arrow keys in the code like normal without the
    completion menu interfeering.

--- nvim/lua/cmp-completion.lua
+++ nvim/lua/cmp-completion.lua
@@ -1,5 +1,5 @@
 local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
@@ -46,13 +46,47 @@ cmp.setup({
 			end
 		end, { "i", "s" }),
 
+		["<Down>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				if cmp.get_selected_entry() then
+					cmp.select_next_item()
+				else
+					fallback()
+				end
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		["<Up>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				if cmp.get_selected_entry() then
+					cmp.select_prev_item()
+				else
+					fallback()
+				end
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		["<Right>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				if cmp.get_selected_entry() then
+					cmp.confirm({ select = false })
+				else
+					fallback()
+				end
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
 		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 		["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
 		["<Esc>"] = close,
-		["<Left>"] = close,
-		["<Right>"] = close,
 		-- Set `select` to `false` to only confirm explicitly selected items.
 		["<CR>"] = cmp.mapping.confirm({ select = false }),
 	},
@@ -72,7 +106,7 @@ cmp.setup({
 
 			-- The function below will be called before any actual modifications from lspkind
 			-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-			before = function(_entry, vim_item)
+			before = function(_, vim_item)
 				return vim_item
 			end,
 		}),
