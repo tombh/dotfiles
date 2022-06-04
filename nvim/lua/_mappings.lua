cc4f2fa7fa2a7ef9c7dfc8e04836977b2293466a
Thomas Buckley-Houston <tom@tombh.co.uk>
2022-06-04 09:51:28 -0300

    nvim: TIL `:` are similar <cmd>, but <cmd> better

--- nvim/lua/_mappings.lua
+++ nvim/lua/_mappings.lua
@@ -8,27 +8,12 @@ _G.keymap = function(key, main_command, selection_command)
 	if string.startswith(main_command, "<") then
 		command = main_command
 	else
-		command = string.format(":%s<CR>", main_command)
+		command = string.format("<cmd>%s<CR>", main_command)
 	end
 
-	vim.api.nvim_set_keymap(
-		"i",
-		key,
-		string.format("<C-O>%s", command),
-		{ noremap = true, silent = true }
-	)
-	vim.api.nvim_set_keymap(
-		"n",
-		key,
-		command,
-		{ noremap = true, silent = true }
-	)
-	vim.api.nvim_set_keymap(
-		"c",
-		key,
-		command,
-		{ noremap = true, silent = true }
-	)
+	vim.api.nvim_set_keymap("i", key, command, { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", key, command, { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("c", key, command, { noremap = true, silent = true })
 
 	if selection_command then
 		main_command = selection_command
@@ -40,18 +25,8 @@ _G.keymap = function(key, main_command, selection_command)
 		command = string.format(":'<,'>%s<CR>", main_command)
 	end
 
-	vim.api.nvim_set_keymap(
-		"v",
-		key,
-		command,
-		{ noremap = true, silent = true }
-	)
-	vim.api.nvim_set_keymap(
-		"s",
-		key,
-		string.format("<C-O>%s", command),
-		{ noremap = true, silent = true }
-	)
+	vim.api.nvim_set_keymap("v", key, command, { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("s", key, string.format("<C-O>%s", command), { noremap = true, silent = true })
 end
 
 function _G.buffer_switch_mru()
